bca54970b7416020a2975180c8b77e44dea7b0c6
Daniel Arao <araodaniel14@gmail.com>
2023-06-07 15:33:37 -0400

    .text added

--- century.py
+++ century.py
@@ -27,8 +27,8 @@ inventario.click() #.sendkeys('madrid')
 pais = WebDriverWait(driver, 60).until(ec.element_to_be_clickable((By.XPATH,'//*[@id="filterSidebar"]/div/div[2]/fieldset[2]/div[2]/div/button')))
 pais.click() #.sendkeys('madrid')
 
-#Check box venezuela: //*[@id="react-select-14-option-0"]/input //*[@id="react-select-20-option-0"]/input //*[@id="react-select-13-option-0"]/input
-#react-select-13-option-0 > input[type=checkbox] #react-select-13-option-0 > input[type=checkbox] #react-select-14-option-0 > input[type=checkbox]
+#Check box venezuela: 
+#react-select-13-option-0 > input[type=checkbox] 
 check = WebDriverWait(driver, 60).until(ec.element_to_be_clickable((By.CSS_SELECTOR,'#react-select-13-option-0 > input[type=checkbox]')))
 check.click() 
 
@@ -44,14 +44,54 @@ anzoategui.click()
 citybutton = WebDriverWait(driver, 60).until(ec.element_to_be_clickable((By.XPATH,'/html/body/div[2]/div/div/div/main/div/div[1]/div[2]/div/div[1]/div/div[2]/fieldset[4]/div[2]/div/button')))
 citybutton.click()
 
-
 #lecheria /html/body/div[2]/div/div/div/main/div/div[1]/div[2]/div/div[1]/div/div[2]/fieldset[4]/div[2]/div/div[1]/div/div[2]/div/div[20]/input
 lecheria =driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div/main/div/div[1]/div[2]/div/div[1]/div/div[2]/fieldset[4]/div[2]/div/div[1]/div/div[2]/div/div[20]/input')
 driver.execute_script("window.scrollTo(0,document.body.scrollHeight)",lecheria)
 lecheria.click()
 
+time.sleep(5)
 
-#//*[@id="main"]/div[1]/div[1]/h5
-totalresults = WebDriverWait(driver, 60).until(ec.element_to_be_clickable((By.XPATH,'//*[@id="main"]/div[1]/div[1]/h5')))
-totalresults.text()
-print(totalresults)
+## Datos a guardar
+## Total de resultados  /html/body/div[2]/div/div/div/main/div/div[1]/div[2]/div/div[2]/div[1]/div[1]/h5
+
+## Tipo #main > div.row.mt-4 > div:nth-child(2) > div > div.d-flex.flex-wrap.flex-xxl-nowrap.justify-content-center > div.flex-grow-1.d-flex.flex-column.bd-highlight.mb-3.position-relative > div.text-truncate.text-capitalize.mb-0.small.pb-1.text-info.fw-bold
+tipovivienda = driver.find_element(By.CSS_SELECTOR, 'main > div.row.mt-4 > div:nth-child(2) > div > div.d-flex.flex-wrap.flex-xxl-nowrap.justify-content-center > div.flex-grow-1.d-flex.flex-column.bd-highlight.mb-3.position-relative > div.text-truncate.text-capitalize.mb-0.small.pb-1.text-info.fw-bold')
+tipovivienda = tipovivienda.text
+
+## Titulo #main > div.row.mt-4 > div:nth-child(2) > div > div.d-flex.flex-wrap.flex-xxl-nowrap.justify-content-center > div.flex-grow-1.d-flex.flex-column.bd-highlight.mb-3.position-relative > a
+titulo_vivienda = driver.find_element(By.CSS_SELECTOR, '#main > div.row.mt-4 > div:nth-child(2) > div > div.d-flex.flex-wrap.flex-xxl-nowrap.justify-content-center > div.flex-grow-1.d-flex.flex-column.bd-highlight.mb-3.position-relative > a')
+titulo_vivienda = titulo_vivienda.text
+
+## Ubicacion  #main > div.row.mt-4 > div:nth-child(2) > div > div.d-flex.flex-wrap.flex-xxl-nowrap.justify-content-center > div.flex-grow-1.d-flex.flex-column.bd-highlight.mb-3.position-relative > div.text-muted.font-weight-light.text-truncate.small.m-0
+ubicacion = driver.find_element(By.CSS_SELECTOR, '#main > div.row.mt-4 > div:nth-child(2) > div > div.d-flex.flex-wrap.flex-xxl-nowrap.justify-content-center > div.flex-grow-1.d-flex.flex-column.bd-highlight.mb-3.position-relative > div.text-muted.font-weight-light.text-truncate.small.m-0')
+ubicacion = ubicacion.text
+
+##Terreno en m2 #main > div.row.mt-4 > div:nth-child(2) > div > div.d-flex.flex-wrap.flex-xxl-nowrap.justify-content-center > div.flex-grow-1.d-flex.flex-column.bd-highlight.mb-3.position-relative > div.mt-2.text-muteds.text-truncate.small > span:nth-child(1) > span
+terreno = driver.find_element(By.CSS_SELECTOR,'#main > div.row.mt-4 > div:nth-child(2) > div > div.d-flex.flex-wrap.flex-xxl-nowrap.justify-content-center > div.flex-grow-1.d-flex.flex-column.bd-highlight.mb-3.position-relative > div.mt-2.text-muteds.text-truncate.small > span:nth-child(1) > span')
+terreno = terreno.text
+
+##COnstruccion en m2 #main > div.row.mt-4 > div:nth-child(2) > div > div.d-flex.flex-wrap.flex-xxl-nowrap.justify-content-center > div.flex-grow-1.d-flex.flex-column.bd-highlight.mb-3.position-relative > div.mt-2.text-muteds.text-truncate.small > span:nth-child(3) > span
+construccion = driver.find_element(By.CSS_SELECTOR,'#main > div.row.mt-4 > div:nth-child(2) > div > div.d-flex.flex-wrap.flex-xxl-nowrap.justify-content-center > div.flex-grow-1.d-flex.flex-column.bd-highlight.mb-3.position-relative > div.mt-2.text-muteds.text-truncate.small > span:nth-child(3) > span')
+construccion = construccion.text
+
+## habitaciones /html/body/div[2]/div/div/div/main/div/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div[2]/div[4]/span[1]/text()[1]
+habitaciones = driver.find_element(By.XPATH,'/html/body/div[2]/div/div/div/main/div/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div[2]/div[4]/span[1]/text()[1]')
+habitaciones = habitaciones.text
+
+## baños /html/body/div[2]/div/div/div/main/div/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div[2]/div[4]/span[2]/text()[1]
+banos = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/main/div/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div[2]/div[4]/span[2]/text()[1]')
+banos = banos.text
+
+## estacionamientos /html/body/div[2]/div/div/div/main/div/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div[2]/div[4]/span[3]/text()[1]
+estacionamientos = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/main/div/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div[2]/div[4]/span[3]/text()[1]')
+estacionamientos = estacionamientos.text
+
+## Piscina(conertir a bool)  /html/body/div[2]/div/div/div/main/div/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div[2]/div[4]/span[4]/text()
+
+## Precio  /html/body/div[2]/div/div/div/main/div/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div[2]/div[5]/div/h5
+precio = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/main/div/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div[2]/div[5]/div/h5')
+precio = precio.text
+
+## Link  /html/body/div[2]/div/div/div/main/div/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div[2]/div[6]/div/div/div[2]/a[2]
+link = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/main/div/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div[1]/div[2]/div[6]/div/div/div[2]/a[2]')
+link.text
