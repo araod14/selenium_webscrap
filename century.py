e8f0be6cfed8a699a4ee145a4333e60656f5d5de
Daniel Arao <araodaniel14@gmail.com>
2023-07-02 22:06:19 -0400

    info capturada, falta limpieza

--- century.py
+++ century.py
@@ -53,56 +53,16 @@ time.sleep(5)
     ## Datos a guardar
 casas = driver.find_elements(By.XPATH, '//div[@class="col scrollbar"]/div[@class="row mt-4"]/div[@class="col-md-6"]')
 for i in casas:
-    #tipoventas = driver.find_elements(By.XPATH, '//div[@class="flex-grow-1 d-flex flex-column bd-highlight mb-3 position-relative"]/div[@class="text-truncate text-capitalize mb-0 small pb-1 text-info fw-bold "]')
-    #tipo = [tipoventa.text for tipoventa in tipoventas]
-
-    #tipoviviendas = driver.find_elements(By.XPATH, '//div[@class="flex-grow-1 d-flex flex-column bd-highlight mb-3 position-relative"]/a[@class="text-decoration-none h5 link-dark text-truncate text-capitalize mb-0 stretched-link "]')
-    #viviendas = [tipovivienda.text for tipovivienda in tipoviviendas]
-
-    #precios = driver.find_elements(By.XPATH, '//div[@class="flex-grow-1 d-flex flex-column bd-highlight mb-3 position-relative"]/div[@class="row  justsify-content-between mt-auto"]/div[@class="col-12"]/h5')
-    #precio = [precio.text for precio in precios]
-
-    #ubicaciones = driver.find_elements(By.XPATH, '//div[@class="flex-grow-1 d-flex flex-column bd-highlight mb-3 position-relative"]/div[@class="text-muted font-weight-light text-truncate small m-0"]')
-    #direccion = [ubicacion.text for ubicacion in ubicaciones]
-
-    #terrenos = driver.find_elements(By.XPATH,'//div[@class="flex-grow-1 d-flex flex-column bd-highlight mb-3 position-relative"]/div[@class="mt-2 text-muteds text-truncate small"]/span[@class=""][1]/span[@class="fw-bolds"]')
-    #terreno = [terreno.text for terreno in terrenos]
-
-    #construcciones = driver.find_elements(By.XPATH,'//div[@class="flex-grow-1 d-flex flex-column bd-highlight mb-3 position-relative"]/div[@class="mt-2 text-muteds text-truncate small"]/span[@class=""][2]/span[@class="fw-bolds"]')
-    #construccion = [construccion.text if construccion else '' for construccion in construcciones]
-
-    #habitaciones = driver.find_elements(By.XPATH,'//div[@class="flex-grow-1 d-flex flex-column bd-highlight mb-3 position-relative"]/div[@class="mb-2 text-muteds text-truncate small"]/span[@class="me-3"][1]')
-    #hab = [habitacion.text for habitacion in habitaciones]
-
-    banos = driver.find_elements(By.XPATH, '//div[@class="d-flex flex-wrap flex-xxl-nowrap justify-content-center"]')
-    ban = [bano.text for bano in banos]
-    """
-    ban = []
-    for i in banos:
-        if i.text=='':
-            ban.append('no')
-        elif i.text:
-            ban.append(i.text)
-        else:
-            ban.append('no')
-    """
-
-    #estacionamientos = driver.find_elements(By.XPATH, '//div[@class="flex-grow-1 d-flex flex-column bd-highlight mb-3 position-relative"]/div[@class="mb-2 text-muteds text-truncate small"]/span[@class="me-3"][3]')
-    #estacion = [estacionamiento.text if estacionamiento.text else None for estacionamiento in estacionamientos]
-
-    #pools = driver.find_elements(By.XPATH, '//div[@class="flex-grow-1 d-flex flex-column bd-highlight mb-3 position-relative"]/div[@class="mb-2 text-muteds text-truncate small"]/span[@class="me-3"][5]')
-    #piscina = [True if pool.text == 'Piscina' else False if pool.text != None else False for pool in pools]
+    viviendas = driver.find_elements(By.XPATH, '//div[@class="d-flex flex-wrap flex-xxl-nowrap justify-content-center"]')
+    vivi = [vivienda.text for vivienda in viviendas]
 
     #links = driver.find_elements(By.XPATH, '//div[@class="col text-end"]/a')
     #link = [link.get_attribute("href") for link in links]
 
-    
-#df = pd.DataFrame({
- #   "tipo":tipo, "viviendas":viviendas, "precio":precio,
- #   "direccion":direccion, "terreno":terreno})
- #   #"construccion":construccion,
+#df = pd.DataFrame({"viviendas":ban})
+    #"construccion":construccion,
     #"habitaciones":hab, "banos":bano, "estacionamientos":estacion,
     #"link":link
 
-#df.to_csv('viviendas_lecheria.csv', index=False)
-print(ban)
+#df.to_csv('viviendas_.csv', index=False)
+print(vivi)
