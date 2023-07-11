a35dc6e30aac2210197d97b80a1508a608e3fff2
Daniel Arao <araodaniel14@gmail.com>
2023-07-11 19:03:20 -0400

    completado

--- home24.py
+++ home24.py
@@ -49,3 +49,36 @@ busqueda = WebDriverWait(driver, 60).until(ec.element_to_be_clickable((By.XPATH,
 busqueda.click()
 
 time.sleep(5)
+
+listasdepaginas = driver.find_elements(By.XPATH,'//div[@class="pagination"]/ul[@class="pagination"]/li')
+
+def info():
+    casas = driver.find_elements(By.XPATH, '//div[@class="col-12 col-sm-6 col-md-6 col-lg-4 mt-3"]')
+    vivi=[]
+    for casa in casas:
+        viviendas = driver.find_elements(By.XPATH, '//div[@class="col-12 col-sm-6 col-md-6 col-lg-4 mt-3"]')
+        links = driver.find_elements(By.XPATH, '//div[@class="carousel-item active"]/a')
+        vivi = [vivienda.text for vivienda in viviendas]
+        links_to = [link.get_attribute("href") for link in links]
+    return vivi
+
+df = pd.DataFrame({"viviendas":info()})
+#next = driver.find_element(By.XPATH, '//li[@class="page-item"]')
+
+for i in range(3, 9):
+#for i in range(6):
+    page2 = WebDriverWait(driver, 60).until(ec.element_to_be_clickable((By.XPATH, f'/html/body/form/div/div[3]/div[2]/div/div/ul/li[{i}]/a')))
+    page2.click()
+
+    df2 = pd.DataFrame({"viviendas":info()})
+    df = pd.concat([df, df2], axis=0)
+
+for i in range(10, 39):
+    page9 = WebDriverWait(driver, 60).until(ec.element_to_be_clickable((By.XPATH, '/html/body/form/div/div[3]/div[2]/div/div/ul/li[9]/a')))
+    page9.click()
+    df9 = pd.DataFrame({"viviendas":info()})
+    df = pd.concat([df, df9], axis=0)
+
+
+df.to_csv('viviendas_home24.csv', index=False)
+print(df)
