9a55c09ea9b8cbd71bc30bde069b97bf5e11a96b
Daniel Arao <araodaniel14@gmail.com>
2023-08-30 15:31:29 -0400

    raw yamaha listo

--- buy2race.py
+++ buy2race.py
@@ -3,25 +3,52 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+
 import pandas as pd
+
 import time
 
-#Path de chromedriver
-path = '/home/danel149/chromedriver'
 
 service = Service()
-#Opciones de chrome
 options = webdriver.ChromeOptions()
-##Estas linea solo para trabajar desde pdvsa
-#options.add_argument('--disable-extensions')
-#proxy = "10.172.31.3:8000"
-#options.add_argument('--proxy-server=%s' % proxy)
-
 driver = webdriver.Chrome(service=service, options=options)
 
 #Iniciar el navegador
 driver.get('https://buy2race.com/es/')
 
 moto='Yamaha XTZ 700 TENERE'
-buscarmoto = WebDriverWait(driver, 60).until(ec.element_to_be_clickable((By.XPATH,'//*[@id="search_widget"]/form/input[2]')))
+
+buscador = WebDriverWait(driver, 60).until(ec.element_to_be_clickable((By.XPATH,'//*[@id="search_widget"]/form/input[2]')))
+buscador.click()#send_keys(moto)
+
+buscarmoto = WebDriverWait(driver, 60).until(ec.element_to_be_clickable((By.XPATH,'//*[@id="df-searchbox__dffullscreen"]')))
 buscarmoto.send_keys(moto)
+
+time.sleep(3)
+
+def info():
+    ## Datos a guardar
+    result = []
+    links = driver.find_elements(By.XPATH, '//div[@class="df-card"]/a')
+    titles = driver.find_elements(By.XPATH, '//div[@class="df-card__title"]')
+    availitys = driver.find_elements(By.XPATH, '//div[@class="df-card__pricing"]/span/span')
+    prices = driver.find_elements(By.XPATH, '//span[@class="df-card__price "]')
+        
+    title_l = [title.text for title in titles]
+    availities_l = [availity.get_attribute("class") for availity in availitys]
+    prices_l = [price.text for price in prices]
+    links_to = [link.get_attribute("href") for link in links]
+
+    result.extend(title_l)
+    result.extend(availities_l)
+    result.extend(prices_l)
+    result.extend(links_to)
+
+    return result
+
+df = pd.DataFrame({"Yamaha XTZ 700 TENERE":info()})
+
+df.to_csv('ymaha_xtz_700_tenere_raw.csv', index=False)
+
+print(df)
+
